80512c8b771225fd92f11e1a4e3ba604c12aed6a
beautigupta <beautirsp2003@gmail.com>
2026-02-11 20:52:01 +0530

    update

--- src/main/java/com/my/spring/main.java
+++ src/main/java/com/my/spring/main.java
@@ -9,20 +9,24 @@ import com.my.spring.model.Employee;
 public class main {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
 //		
-		Employee bean = ioc.getBean("emp1",Employee.class);
-//		
+		Employee bean = ioc.getBean("emp1", Employee.class);
 		System.out.println(bean);
-		
-		
+		bean = ioc.getBean("emp1", Employee.class);
+		System.out.println(bean);
+		bean = ioc.getBean("emp1", Employee.class);
+		System.out.println(bean);
+		bean.setName("shubham");
+		bean.setGender("male");
+		System.out.println(bean);
+
 //		Employee bean2 = ioc.getBean("emp2",Employee.class);
 //
 //		System.out.println(bean2);
 //		
 //		System.out.println("Main.main()");
-		
 
 	}
 
