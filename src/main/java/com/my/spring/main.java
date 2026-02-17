4d2b1e3695be9aeffa43e8c292a67c7f187ae758
beautigupta <beautirsp2003@gmail.com>
2026-02-17 22:11:37 +0530

    update

--- src/main/java/com/my/spring/main.java
+++ src/main/java/com/my/spring/main.java
@@ -2,32 +2,22 @@ package com.my.spring;
 
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.my.spring.model.Address;
 import com.my.spring.model.Employee;
 
 public class main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
-//		
-		Employee bean = ioc.getBean("emp1", Employee.class);
-		System.out.println(bean);
-		bean = ioc.getBean("emp1", Employee.class);
-		System.out.println(bean);
-		bean = ioc.getBean("emp1", Employee.class);
-		System.out.println(bean);
-		bean.setName("shubham");
-		bean.setGender("male");
-		System.out.println(bean);
+		ApplicationContext ioc = new AnnotationConfigApplicationContext("com.my.spring.model");
 
-//		Employee bean2 = ioc.getBean("emp2",Employee.class);
-//
-//		System.out.println(bean2);
-//		
-//		System.out.println("Main.main()");
-
+		Employee emp = ioc.getBean(Employee.class);
+		
+		System.out.println(emp);
+		
+		
 	}
 
 }
---
+++ src/main/java/com/my/spring/model/lAddress.java
@@ -0,0 +1,17 @@
+package com.my.spring.model;
+
+
+
+
+public interface lAddress {
+	public String getCity();
+
+	public void setCity(String city);
+
+	public String getState();
+
+	public void setState(String state);
+}
+
+
+
